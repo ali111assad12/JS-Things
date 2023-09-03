f503fd38d5a02a0ba0102ba9c60cd31a9870abee
ali111assad12 <143921838+ali111assad12@users.noreply.github.com>
2023-09-03 06:48:22 +0000

    kuchbhe

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// "use strict"; // treat all Js code as newer version
+
+// alert("hello")  works in node js not browsers
+
+// console.log(3-2) 
+// console.log("hello")
+
+
+// let name = "hitest"
+ let age = 19
+
+// primitive data types
+// datatypes  strings""" boolean true/false 
+// bigint
+// null= standalone value
+// undefined= value not assinged
+//  symbol= unique
+
+
+// object
+
+console.log(typeof "age");
+
