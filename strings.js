92d8d81e8d016d5c7c72684141c6ce2c92f40ea2
ali111assad12 <143921838+ali111assad12@users.noreply.github.com>
2023-09-03 13:02:35 +0000

    stringds

---
+++ strings.js
@@ -0,0 +1,22 @@
+const name = "hitesh"
+const Repocount = 50
+
+// console.log(name + Repocount + "value");
+console.log(`hello my name is ${name}and my repo count is ${Repocount}`);
+ const gameName = new String('hites-hc')
+
+
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+
+
+ console.log (gameName);
+ console.log(gameName.charAt(3));
+ console.log(gameName.indexOf(3));
+
+ const newString = gameName.substring(0, 4)
+ console.log(newString);
+ const anotherString = gameName.slice(-8, 5)
+
+console.log(anotherString)
+
