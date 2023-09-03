df6955520aee4340423c46c296e85b2e12eede79
ali111assad12 <143921838+ali111assad12@users.noreply.github.com>
2023-09-03 06:48:11 +0000

    datatypes

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 122
+let accountEmail = "ali111assad@lolmail.com"
+var accountPassword = "123"
+accountcity = "Jaipur"
+let accountState;
+
+// accountId = 2// not allowed
+accountEmail = "kuchbhee@gmail.com"
+accountPassword = 1234
+/*
+perfer not to use var
+*/
+console.log(accountId); 
+console.table([accountEmail,accountPassword,accountState])
+
+
