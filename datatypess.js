1027f18141a2060f10ce3d886a47f18f1266fd58
ali111assad12 <143921838+ali111assad12@users.noreply.github.com>
2023-09-03 10:03:48 +0000

    datatyoesss

---
+++ datatypess.js
@@ -0,0 +1,16 @@
+// premitive   and Non Premitive
+
+// premitive data types
+// 7 types 
+//string,nubmer,boolean,null,
+// undefined,symbol,Bigint
+//  
+// Non Premitive data types
+// arrays, Objects, Functions
+ const heros = [shaktiman,superman]
+ let myObj = {
+    Name: "hitest"
+    age: "23"
+ }
+
+ console.log(typeof bigNumber);
--- operations.js
+++ operations.js
@@ -11,5 +11,41 @@ let str1 ="hello"
 let str2 = "hitesh"
 
 let str3 = str1 + str2
+console.log(3+2+"30")
+console.log ((4-3)*5*2)
+
+console.log(+"")
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+console.log(2>3);
+
+
+console.log("2">1);
+console.log("02">1);
+console.log("null">1);
+
+
+console.log("null">0);
+console.log("null"==0);
+console.log("null">=0);
+
+console.log(undefined==0);
+
+
+
+// ===
+
+
+console.log("2"===2);
+
+
+
+// datatypes
+// 
+
+
+
 
 
